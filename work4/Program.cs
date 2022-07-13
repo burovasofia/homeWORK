4f90a48dc9a096d721d524c26e7ce4e99f56afe3
Sofia <burovasofia@list.ru>
2022-07-13 18:44:21 +0300

    Задача4

---
+++ work4/Program.cs
@@ -0,0 +1,16 @@
+﻿int num;
+Console.Write("Введите число ");
+num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while (current <= num)
+{
+ if(current % 2 == 0)
+    {
+        Console.Write(current + " ");
+    }
+    else
+    {
+    }
+    current = current + 1;
+}
+
