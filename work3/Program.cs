c95e046fbf1f4d5cdb29627bd4cd12a7cc0adf6c
Sofia <burovasofia@list.ru>
2022-07-13 18:33:36 +0300

    Задача3

---
+++ work3/Program.cs
@@ -0,0 +1,13 @@
+﻿int num;
+
+Console.WriteLine("Введите число");
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число не четное");
+}
