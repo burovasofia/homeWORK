744390b9eb03cfe2fcb36496cfe38bea7d7b1287
Sofia <burovasofia@list.ru>
2022-07-13 18:09:44 +0300

    Задача2 изменения

--- work2/Program.cs
+++ work2/Program.cs
@@ -9,23 +9,22 @@ num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 num3 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
+if(num2 > num3)
 {
-    Console.WriteLine("максимальное число " + num1);
-    if(num1 > num3)
+    if(num1 > num2)
      {
 Console.WriteLine("максимальное число " + num1);
      }
     else
     {
-        Console.WriteLine("максимальное число " + num3);
+        Console.WriteLine("максимальное число " + num2);
     }
 }
 else
 {
-    if(num2 > num3)
+    if(num1 > num3)
     {
-        Console.WriteLine("максимальное число " + num2);
+        Console.WriteLine("максимальное число " + num1);
     }
     else
     {
