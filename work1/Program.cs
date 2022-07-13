49230c1298ece1a09a52900d2e8f7165c8e76fe3
Sofia <burovasofia@list.ru>
2022-07-13 13:08:39 +0300

    Задача1

---
+++ work1/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1, num2;
+
+Console.WriteLine("Введите первое число");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("max = num1, min = num2");
+}
+else
+{
+    Console.WriteLine("max = num2, min = num1");
+}
