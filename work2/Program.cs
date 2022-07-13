cb70c0a3e126640b3e56ab10ad096ee355b6872b
Sofia <burovasofia@list.ru>
2022-07-13 17:47:48 +0300

    Задача2

---
+++ work2/Program.cs
@@ -0,0 +1,34 @@
+﻿int num1, num2, num3;
+
+Console.WriteLine("Введите первое число");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("максимальное число " + num1);
+    if(num1 > num3)
+     {
+Console.WriteLine("максимальное число " + num1);
+     }
+    else
+    {
+        Console.WriteLine("максимальное число " + num3);
+    }
+}
+else
+{
+    if(num2 > num3)
+    {
+        Console.WriteLine("максимальное число " + num2);
+    }
+    else
+    {
+        Console.WriteLine("максимальное число " + num3);
+    }
+}
